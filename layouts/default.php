9e70504471b37bced8e6da8219770f7a5c512006
MagicznyFrank <92795337+MagicznyFrank@users.noreply.github.com>
2021-11-17 18:56:39 +0100

    Add files via upload

--- layouts/default.php
+++ layouts/default.php
@@ -1,17 +1,14 @@
 <html>
-    <head>
-        <title>
-            <?php echo $title; ?>
-        </title>
-    </head>
-
-    <body>
-        <h3>HEJ</h3>
-        <a href="/">Home Page</a>
-        <br>
-        <a href="/body">Body Page</a>
-        <br>
-        <a href="/article">Article Page</a>
-        <?php echo $content; ?>
-    </body>
+<head>
+	<title><?php echo $title ?></title>
+</head>
+<body>
+<body>
+<h1>Hello APSL!</h1>
+<a href="<?php echo $router->generate('home') ?>"></a>
+<a href="<?php echo $router->generate('body') ?>">BODY Page</a>
+<a href="<?php echo $router->generate('article', ['id' => 2]) ?>">Article Page</a>
+<?php echo $content ?>
+</body>
+</body>
 </html>
